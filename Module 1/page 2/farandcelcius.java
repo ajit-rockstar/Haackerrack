6c53c6860dfba9475c8799eacde8c3e0911c8bb3
ajit <ajitchaubey1998@gmail.com>
2023-06-01 19:13:43 +0530

    farandcelcius

---
+++ Module 1/page 2/farandcelcius.java
@@ -0,0 +1,17 @@
+import java.io.*;
+import java.util.*;
+import java.text.*;
+import java.math.*;
+import java.util.regex.*;
+import java.util.Scanner;
+public class farandcelcius {
+
+    public static void main(String[] args) {
+        Scanner sc=new Scanner(System.in);
+        double far = sc.nextDouble();
+        double cel = (((far - 32.0) *5.0)/9.0);
+        System.out.println(cel);
+        
+        /* Enter your code here. Read input from STDIN. Print output to STDOUT. Your class should be named Solution. */
+    }
+}
