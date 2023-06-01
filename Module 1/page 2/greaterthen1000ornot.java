f821d53dbc47949df996f0b92b6b1c7ba85b8b4a
ajit <ajitchaubey1998@gmail.com>
2023-06-01 19:29:57 +0530

    >1000ornot

---
+++ Module 1/page 2/greaterthen1000ornot.java
@@ -0,0 +1,16 @@
+import java.io.*;
+import java.util.*;
+import java.text.*;
+import java.math.*;
+import java.util.regex.*;
+import java.util.Scanner;
+
+public class greaterthen1000ornot {
+
+    public static void main(String[] args) {
+        Scanner sc= new Scanner(System.in);
+        int x=sc.nextInt();
+        System.out.println(x>1000);
+        /* Enter your code here. Read input from STDIN. Print output to STDOUT. Your class should be named Solution. */
+    }
+}
