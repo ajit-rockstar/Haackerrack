573f29c9938b71daae8fc386b48e1477608c7449
ajit <ajitchaubey1998@gmail.com>
2023-06-01 19:06:08 +0530

    sumanddiff

---
+++ sumanddiff.java
@@ -0,0 +1,14 @@
+
+import java.util.Scanner;
+
+public class sumanddiff {
+
+    public static void main(String[] args) {
+        Scanner scn= new Scanner(System.in);
+        int x=scn.nextInt();
+        int y=scn.nextInt();
+        System.out.println(x+y);
+        System.out.println(x-y);
+        /* Enter your code here. Read input from STDIN. Print output to STDOUT. Your class should be named Solution. */
+    }
+}
