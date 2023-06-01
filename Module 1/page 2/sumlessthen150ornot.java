43290d7177947e1bc1c4495074081fa08ae5e893
ajit <ajitchaubey1998@gmail.com>
2023-06-01 19:24:32 +0530

    sum<150ornot

---
+++ Module 1/page 2/sumlessthen150ornot.java
@@ -0,0 +1,19 @@
+import java.io.*;
+import java.util.*;
+import java.text.*;
+import java.math.*;
+import java.util.regex.*;
+import java.util.Scanner;
+
+public class sumlessthen150ornot {
+
+    public static void main(String[] args) {
+        Scanner sc= new Scanner(System.in);
+        int x= sc.nextInt();
+        int y= sc.nextInt();
+        int z= sc.nextInt();
+        int sum= x+y+z;
+        System.out.println(sum<150);
+        /* Enter your code here. Read input from STDIN. Print output to STDOUT. Your class should be named Solution. */
+    }
+}
