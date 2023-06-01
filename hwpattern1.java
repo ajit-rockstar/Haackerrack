9e021a535497bcc8f46b0ce0c8b033c4c22ab15f
ajit <ajitchaubey1998@gmail.com>
2023-06-01 19:03:13 +0530

    hwpattern1

---
+++ hwpattern1.java
@@ -0,0 +1,18 @@
+import java.io.*;
+import java.util.*;
+import java.text.*;
+import java.math.*;
+import java.util.regex.*;
+
+public class hwpattern1 {
+
+    public static void main(String[] args) {
+        System.out.println("Hello");
+        System.out.println("I");
+        System.out.println("am");
+        System.out.println("enjoying");
+        System.out.println("coding");
+            
+        /* Enter your code here. Read input from STDIN. Print output to STDOUT. Your class should be named Solution. */
+    }
+}
