2c8b0668c8eb1960939635c8c931b277c2e8263f
ajit <ajitchaubey1998@gmail.com>
2023-06-01 19:04:36 +0530

    hwstarpattern

---
+++ hwstarpattern.java
@@ -0,0 +1,18 @@
+import java.io.*;
+import java.util.*;
+import java.text.*;
+import java.math.*;
+import java.util.regex.*;
+
+public class hwstarpattern {
+
+    public static void main(String[] args) {
+        System.out.println("*****");
+        System.out.println("*****");
+        System.out.println("*****");
+        System.out.println("*****");
+        System.out.println("*****");
+        System.out.println("*****");
+        /* Enter your code here. Read input from STDIN. Print output to STDOUT. Your class should be named Solution. */
+    }
+}
