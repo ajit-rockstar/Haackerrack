0b38863b7d5d305f42fe9da350681d5f3cbd94de
ajit <ajitchaubey1998@gmail.com>
2023-06-01 19:23:08 +0530

    evenornot

---
+++ Module 1/page 2/evenornot.java
@@ -0,0 +1,16 @@
+import java.io.*;
+import java.util.*;
+import java.text.*;
+import java.math.*;
+import java.util.regex.*;
+import java.util.Scanner;
+
+public class evenornot {
+
+    public static void main(String[] args) {
+        Scanner sc= new Scanner(System.in);
+        int a= sc.nextInt();
+        System.out.println(a%2==0);
+        /* Enter your code here. Read input from STDIN. Print output to STDOUT. Your class should be named Solution. */
+    }
+}
