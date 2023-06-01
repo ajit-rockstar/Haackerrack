144137500318820910390e665aac7b4dd40231b4
ajit <ajitchaubey1998@gmail.com>
2023-06-01 19:32:19 +0530

    adultornot

---
+++ Module 1/page 2/adultornot.java
@@ -0,0 +1,22 @@
+import java.io.*;
+import java.util.*;
+import java.text.*;
+import java.math.*;
+import java.util.regex.*;
+import java.util.Scanner;
+
+public class adultornot {
+
+    public static void main(String[] args) {
+        Scanner sc= new Scanner(System.in);
+        int age= sc.nextInt();
+        if(age>=18){
+            System.out.println("Adult");
+        }
+        else{
+            System.out.println("Below age");
+        }
+        
+        /* Enter your code here. Read input from STDIN. Print output to STDOUT. Your class should be named Solution. */
+    }
+}
