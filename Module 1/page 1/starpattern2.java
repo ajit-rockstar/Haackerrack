de5458610363848a15324302fe2a0d55b673f10c
ajit <ajitchaubey1998@gmail.com>
2023-06-01 18:57:13 +0530

    starpattern2

---
+++ Module 1/page 1/starpattern2.java
@@ -0,0 +1,17 @@
+import java.io.*;
+import java.util.*;
+import java.text.*;
+import java.math.*;
+import java.util.regex.*;
+
+public class starpattern2 {
+
+    public static void main(String[] args) {
+        System.out.println("*****");
+        System.out.println("*****");
+        System.out.println("*****");
+        
+        
+        /* Enter your code here. Read input from STDIN. Print output to STDOUT. Your class should be named Solution. */
+    }
+}
