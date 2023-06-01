a7702fe0d4ca5ab3f0e6d51f0da522803e51b994
ajit <ajitchaubey1998@gmail.com>
2023-06-01 18:48:17 +0530

    Hellopattern

---
+++ Module 1/page 2/Helloworldpattern.java
@@ -0,0 +1,20 @@
+package Module 1.page 2;
+
+import java.io.*;
+import java.util.*;
+import java.text.*;
+import java.math.*;
+import java.util.regex.*;
+
+public class Helloworldpattern {
+
+    public static void main(String[] args) {
+        System.out.println("Hello");
+        System.out.println("World.");
+        System.out.println("I");
+        System.out.println("am");
+        System.out.println("here.");
+        
+        /* Enter your code here. Read input from STDIN. Print output to STDOUT. Your class should be named Solution. */
+    }
+}
