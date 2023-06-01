7b57a2f340752e42374f2c50245fcef50c6c1d1c
ajit <ajitchaubey1998@gmail.com>
2023-06-01 19:18:52 +0530

    areaoftriangle

---
+++ Module 1/page 2/areaoftriangle.java
@@ -0,0 +1,17 @@
+import java.io.*;
+import java.util.*;
+import java.text.*;
+import java.math.*;
+import java.util.regex.*;
+import java.util.Scanner;
+public class areaoftriangle {
+
+    public static void main(String[] args) {
+        Scanner sc=new Scanner(System.in);
+        double x=sc.nextDouble();
+        double y=sc.nextDouble();
+        double area=(x*y)/2;
+        System.out.println(area);
+        /* Enter your code here. Read input from STDIN. Print output to STDOUT. Your class should be named Solution. */
+    }
+}
