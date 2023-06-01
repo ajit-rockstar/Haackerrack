a3b1bbe5f35723b06890b181c177e871003aeff8
ajit <ajitchaubey1998@gmail.com>
2023-06-01 18:59:29 +0530

    starpattern3

---
+++ Module 1/page 1/starpattern3.java
@@ -0,0 +1,19 @@
+import java.io.*;
+import java.util.*;
+import java.text.*;
+import java.math.*;
+import java.util.regex.*;
+
+public class starpattern3 {
+
+    public static void main(String[] args) {
+        System.out.println("*****");
+        System.out.println("*");
+        System.out.println("*");
+        System.out.println("*");
+        System.out.println("*****");
+        
+        
+        /* Enter your code here. Read input from STDIN. Print output to STDOUT. Your class should be named Solution. */
+    }
+}
