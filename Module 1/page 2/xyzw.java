43ae34ee04f5877af2b8bba8566c6d9cc11deb16
ajit <ajitchaubey1998@gmail.com>
2023-06-01 19:21:52 +0530

    xyzw

---
+++ Module 1/page 2/xyzw.java
@@ -0,0 +1,19 @@
+import java.io.*;
+import java.util.*;
+import java.text.*;
+import java.math.*;
+import java.util.regex.*;
+import java.util.Scanner;
+
+public class xyzw {
+
+    public static void main(String[] args) {
+        Scanner sc= new Scanner(System.in);
+        int x= sc.nextInt();
+        int y=sc.nextInt();
+        int z=sc.nextInt();
+        int w=sc.nextInt();
+        System.out.println(x*y==z*w);
+        /* Enter your code here. Read input from STDIN. Print output to STDOUT. Your class should be named Solution. */
+    }
+}
