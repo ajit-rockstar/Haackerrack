037e6f44fce84be1a83ddf09e4283bb38e15ff14
ajit <ajitchaubey1998@gmail.com>
2023-06-01 19:17:17 +0530

    areaofcircle

---
+++ Module 1/page 2/areaofcircle.java
@@ -0,0 +1,19 @@
+import java.io.*;
+import java.util.*;
+import java.text.*;
+import java.math.*;
+import java.util.regex.*;
+import java.util.Scanner;
+
+public class areaofcircle {
+
+    public static void main(String[] args) {
+        Scanner sc=new Scanner(System.in);
+        int D=sc.nextInt();
+        int r=D/2;
+        int Area= (22*r*r)/7;
+        System.out.println(Area);
+        /* Enter your code here. Read input from STDIN. Print output to STDOUT. Your class should be named Solution. */
+    }
+}
+
