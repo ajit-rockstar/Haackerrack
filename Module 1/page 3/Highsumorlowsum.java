3f76e7e5195cf8c9645b2a14181ad35aab292325
ajit <ajitchaubey1998@gmail.com>
2023-06-02 18:47:36 +0530

    Highsumorlowsum

---
+++ Module 1/page 3/Highsumorlowsum.java
@@ -0,0 +1,23 @@
+import java.io.*;
+import java.util.*;
+import java.text.*;
+import java.math.*;
+import java.util.regex.*;
+import java.util.Scanner;
+
+public class Highsumorlowsum {
+
+    public static void main(String[] args) {
+        Scanner sc= new Scanner(System.in);
+        int x= sc.nextInt();
+        int y= sc.nextInt();
+        int sum=x+y;
+        if(sum>=100){
+            System.out.println("High Sum");
+        }
+        else{
+            System.out.println("Low Sum");
+        }
+        /* Enter your code here. Read input from STDIN. Print output to STDOUT. Your class should be named Solution. */
+    }
+}
