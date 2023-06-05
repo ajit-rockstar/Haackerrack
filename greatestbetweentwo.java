d6e60185f43526b6e8203aa45ca6395d815a3494
ajit <ajitchaubey1998@gmail.com>
2023-06-05 07:05:09 +0530

    a>b || b>a

---
+++ greatestbetweentwo.java
@@ -0,0 +1,25 @@
+import java.io.*;
+import java.util.*;
+import java.text.*;
+import java.math.*;
+import java.util.regex.*;
+import java.util.Scanner;
+
+public class greatestbetweentwo {
+
+    public static void main(String[] args) {
+        Scanner sc=new Scanner(System.in);
+        int x=sc.nextInt();
+        int y=sc.nextInt();
+        if(x>y){
+            System.out.println(x);
+        }
+        else if(y>x){
+            System.out.println(y);
+        }
+        else{
+            System.out.println(x);
+        }
+        /* Enter your code here. Read input from STDIN. Print output to STDOUT. Your class should be named Solution. */
+    }
+}
