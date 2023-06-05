c23f77c047d56d5d2b32f8b7faf014a49c7be91e
ajit <ajitchaubey1998@gmail.com>
2023-06-05 07:06:41 +0530

    evenorodd

---
+++ evenorodd.java
@@ -0,0 +1,22 @@
+import java.io.*;
+import java.util.*;
+import java.text.*;
+import java.math.*;
+import java.util.regex.*;
+import java.util.Scanner;
+
+public class evenorodd {
+
+    public static void main(String[] args) {
+        Scanner sc=new Scanner(System.in);
+        int number=sc.nextInt();
+        if(number%2==0){
+            System.out.println("Even");
+        }
+        else
+        {
+            System.out.println("Odd");
+        }
+        /* Enter your code here. Read input from STDIN. Print output to STDOUT. Your class should be named Solution. */
+    }
+}
