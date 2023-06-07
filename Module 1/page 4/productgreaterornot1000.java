01257aa38f3457a23e1b5010d564fdf76c1ed04e
ajit <ajitchaubey1998@gmail.com>
2023-06-07 14:29:57 +0530

    product>1000?

---
+++ Module 1/page 4/productgreaterornot1000.java
@@ -0,0 +1,21 @@
+import java.io.*;
+import java.util.*;
+import java.text.*;
+import java.math.*;
+import java.util.regex.*;
+import java.util.Scanner;
+public class productgreaterornot {
+
+    public static void main(String[] args) {
+        Scanner sc=new Scanner(System.in);
+        int N1= sc.nextInt();
+        int N2= sc.nextInt();
+        int N3= sc.nextInt();
+        int N4= sc.nextInt();
+        int product=N1*N2*N3*N4;
+        
+            System.out.println(product>1000);
+        
+        /* Enter your code here. Read input from STDIN. Print output to STDOUT. Your class should be named Solution. */
+    }
+}
